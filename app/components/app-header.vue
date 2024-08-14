b1c71ba0051d7a42fda8e7fde748ef581730e18b
betogroo <luizhumberto@gmail.com>
2024-08-14 10:22:29 -0300

    feat: create app-header

---
+++ app/components/app-header.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="flex justify-between items-center">
+    <NuxtLink to="/">Finance Tracker</NuxtLink>
+    <div class="flex space-x-4 items-center">
+      <NavBar />
+      <UAvatar
+        src="https://avatars.githubusercontent.com/u/739984?v=4"
+        alt="Avatar"
+      />
+    </div>
+  </header>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,12 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import AppHeader from '~/components/app-header.vue'
+
+  useHead({
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Inter&display=swap',
+        crossorigin: '',
+      },
+    ],
+  })
+</script>
 
 <template>
-  <div class="container mx-auto px-2 bg-slate-100">
-    <header>
-      <NavBar />
-    </header>
-    <main>
+  <div class="container mx-auto px-2 max-w-4xl bg-slate-100">
+    <AppHeader />
+    <main class="my-10">
       <slot />
     </main>
   </div>
 </template>
+
+<style>
+  /* Set the font family for the entire site */
+  body {
+    font-family: 'Inter';
+  }
+  /* Set the background color for the entire site */
+  body {
+    @apply dark:bg-gray-900 bg-white;
+  }
+</style>
