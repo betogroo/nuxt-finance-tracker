02f6f8b08175e8a82898c041a966c801b9f3ad0d
betogroo <luizhumberto@gmail.com>
2024-08-14 10:58:29 -0300

    feat: create sumary selection

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+  import { transactionViewOptions } from '~/constants'
+
   definePageMeta({
     showInNavBar: true,
     order: 0,
     requiresAuth: true,
     title: 'Home',
   })
+
+  const selectedView = ref(transactionViewOptions[2])
 </script>
 
 <template>
-  <div>Home</div>
+  <section class="flex justify-between items-center mb-0">
+    <h1 class="text-4xl font-extrabold">Sumary</h1>
+    <div>
+      <USelectMenu
+        v-model="selectedView"
+        :options="transactionViewOptions"
+      />
+    </div>
+  </section>
 </template>
