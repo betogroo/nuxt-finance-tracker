4124e484225c13323f258c5d03903162d1cc6e65
betogroo <luizhumberto@gmail.com>
2024-08-16 15:24:37 -0300

    fix: add type parameter to transaction interface

--- app/models/finace-tracker.ts
+++ app/models/finace-tracker.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const transaction = z.object({
   id: z.number().optional(),
   created_at: z.string().nullish(),
+  type: z.string(),
   amount: z.number(),
   description: z.string().optional(),
   category: z.string().optional(),
