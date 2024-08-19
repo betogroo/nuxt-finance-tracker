734d6a4476041e16d1a108604d4e4848ce7032d0
betogroo <luizhumberto@gmail.com>
2024-08-19 10:42:13 -0300

    chore: install eslint module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/supabase'],
+  modules: ['@nuxt/ui', '@nuxtjs/supabase', '@nuxt/eslint'],
   future: {
     compatibilityVersion: 4,
   },
