e191ee2b64faa7163d47d286f621796f70406080
betogroo <luizhumberto@gmail.com>
2024-08-22 10:39:23 -0300

    feat: create add transaction moda

---
+++ app/components/modal/add-transaction.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  const isOpen = ref(false)
+</script>
+
+<template>
+  <UModal v-model="isOpen">
+    <UCard>
+      <template #header> Add Transaction </template>
+      <div>Hello</div>
+      <template #footer />
+    </UCard>
+  </UModal>
+</template>
