943e487c1266ed5831e8f758b5483c4fe573448d
betogroo <luizhumberto@gmail.com>
2024-08-22 15:01:39 -0300

    feat: create transaction form

--- app/components/modal/add-transaction.vue
+++ app/components/modal/add-transaction.vue
@@ -1,12 +1,68 @@
 <script setup lang="ts">
-  const isOpen = ref(false)
+  import { categories, types } from '~/constants'
+  const isOpen = ref(true)
 </script>
 
 <template>
   <UModal v-model="isOpen">
     <UCard>
-      <template #header> Add Transaction </template>
-      <div>Hello</div>
+      <template #header> Adicionar Registro </template>
+      <UFormGroup
+        class="mb-4"
+        label="Tipo de Registro"
+        name="type"
+        :required="true"
+      >
+        <USelect
+          :options="types"
+          placeholder="Selecione o tipo de registro"
+        />
+      </UFormGroup>
+      <UFormGroup
+        class="mb-4"
+        label="Valor"
+        name="amount"
+        :required="true"
+      >
+        <UInput
+          placeholder="Valor"
+          type="number"
+          variant="outline"
+      /></UFormGroup>
+      <UFormGroup
+        class="mb-4"
+        label="Data"
+        name="created_at"
+        :required="true"
+      >
+        <UInput
+          icon="i-heroicons-calendar-days-20-solid"
+          type="date"
+          variant="outline"
+      /></UFormGroup>
+      <UFormGroup
+        class="mb-4"
+        hint="Opcional"
+        label="Descrição"
+        name="description"
+      >
+        <UInput
+          placeholder="Descrição"
+          variant="outline"
+      /></UFormGroup>
+      <UFormGroup
+        class="mb-4"
+        label="Categoria"
+        name="category"
+        :required="true"
+      >
+        <USelect :options="categories" />
+      </UFormGroup>
+      <UButton
+        color="black"
+        type="submit"
+        >Adicionar</UButton
+      >
       <template #footer />
     </UCard>
   </UModal>
